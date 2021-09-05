36bb92e587c8714985a1f97a6acd26597e56c2ab
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 13:32:48 +0600

    Main class

---
+++ Chapter 1/Exercise 1.11/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        double pop = 312032486;
+        double year = 365;
+        System.out.println("1 year later: "+ pop+pop/7+pop/45-pop/13);
+        pop = pop+pop/7+pop/45-pop/13;
+        System.out.println("2 years later: "+ pop+pop/7+pop/45-pop/13);
+        pop = pop+pop/7+pop/45-pop/13;
+        System.out.println("3 years later: "+ pop+pop/7+pop/45-pop/13);
+        pop = pop+pop/7+pop/45-pop/13;
+        System.out.println("4 years later: "+ pop+pop/7+pop/45-pop/13);
+        pop = pop+pop/7+pop/45-pop/13;
+        System.out.println("5 years later: "+ pop+pop/7+pop/45-pop/13);
+        pop = pop+pop/7+pop/45-pop/13;
+    }
+}
