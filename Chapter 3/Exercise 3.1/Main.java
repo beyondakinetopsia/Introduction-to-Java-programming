8943e7230090c163ca3ddd504b586c90293b1e16
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 15:56:56 +0600

    Main class

--- Chapter 3/Exercise 3.1/Main.java
+++ Chapter 3/Exercise 3.1/Main.java
@@ -1,1 +1,12 @@
+import java.util.*;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);   
+        System.out.print("Enter a, b, c: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        System.out.println("The equation has two roots "+ ((-1)*b+Math.pow(b*b-4*a*c))/(2*a) + " and " + ((-1)*b-Math.pow(b*b-4*a*c))/(2*a);
+    }
+}
