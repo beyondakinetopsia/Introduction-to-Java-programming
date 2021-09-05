66496f0df3f15ed3d56f5240eb4846b8b6d15bb2
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 13:31:30 +0600

    Update Main.java

--- Chapter 1/Exercise 1.9/Main.java
+++ Chapter 1/Exercise 1.9/Main.java
@@ -1,6 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println(2*5.5*Math.PI);
-        System.out.println(5.5*5.5*Math.PI);
+        System.out.println(4.5*7.9);
     }
 }
