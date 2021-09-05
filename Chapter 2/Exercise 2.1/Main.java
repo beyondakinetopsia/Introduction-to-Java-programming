d46953c5fec0877200e8d7ed01bd2144f69dbda5
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 13:42:43 +0600

    Main class

---
+++ Chapter 2/Exercise 2.1/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a degree in Celsius: ");
+        double c = input.nextDouble();
+        System.out.println(9.0*c/5+32);
+    }
+}
