eff0a15ea2419b43f82c51d381e3f33b39a7450b
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 14:00:33 +0600

    Main class

---
+++ Chapter 2/Exercise 2.8/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the time zone offset to GMT: ");
+        int num = input.nextInt();
+        
+        long totalMilliseconds = System.currentTimeMillis();
+
+        long totalSeconds = totalMilliseconds/1000;
+        
+        
+        long totalMinutes = totalSeconds/60;
+
+        long totalHours = totalMinutes/60;
+
+        System.out.println("The current time is "+totalHours+":"+totalMinutes+":"+(totalSeconds%));     
+    }
+}
