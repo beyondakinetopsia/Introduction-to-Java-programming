1b70606f1871939ba7745cdf5502e9267fd4d44c
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 14:55:02 +0600

    Main class

---
+++ Chapter 2/Exercise 2.17/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);   
+        System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F: ");
+        double w = input.nextDouble()*
+        System.out.print("Enter the wind speed (>=2) in miles per hour:");
+        double h = input.nextDouble()* 0.0254;
+        System.out.println("The wind chill index is "+(35.74+0.6215*w-35.75*Math.pow(h,0.16)+0.4275*w*Match.pow(h,0.16));
+    }
+}
