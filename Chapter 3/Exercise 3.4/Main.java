dfad381515bf92f9c8767ea109fccb0f234f0f0b
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 17:42:13 +0600

    Main class

---
+++ Chapter 3/Exercise 3.4/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int num = (int)(System.currentTimeMillis() % 12);
+        switch(num){
+          case 0: System.out.println("January"); break;
+          case 1: System.out.println("February"); break;
+          case 2: System.out.println("March"); break;
+          case 3: System.out.println("April"); break;
+          case 4: System.out.println("May"); break;
+          case 5: System.out.println("June"); break;
+          case 6: System.out.println("Jule"); break;
+          case 7: System.out.println("August"); break;
+          case 8: System.out.println("September"); break;
+          case 9: System.out.println("October"); break;
+          case 10: System.out.println("November"); break;
+          case 11: System.out.println("December"); break;
+          default: ;
+        }
+    }
+}
