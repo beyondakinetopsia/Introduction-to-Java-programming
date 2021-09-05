4c81e26122d978116f44901a9706f85f35753edf
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-05 18:55:11 +0600

    Main class

---
+++ Chapter 3/Exercise 3.5/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);   
+        String[] days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        System.out.print("Enter today's day: ");
+        int w = input.nextInt();
+        System.out.print("Enter the number of days elapsed since today: ");
+        int v = input.nextInt();
+
+        w %= 7;
+        System.out.print("Today is" + days[w] "and the future day is " + days[(w+v)%7]);
+    }
+}
