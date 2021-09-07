c70925f1589c61ab61ba7aa145a9f00de83260f1
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-07 12:34:42 +0600

    Main class

--- Chapter 2/Exercise 2.19/Main.java
+++ Chapter 2/Exercise 2.19/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);   
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter three points for a triangle: ");
         double x1 = input.nextDouble();
         double y1 = input.nextDouble();
@@ -10,7 +10,10 @@ public class Main{
         double y2 = input.nextDouble();
         double x3 = input.nextDouble();
         double y3 = input.nextDouble();
-        double s = (Math.pow(Math.pow((x2-x1),2)+Math.pow((y2-y1),2),0.5) + Math.pow(Math.pow((x3-x2),2)+Math.pow((y3-y2),2),0.5) + Math.pow(Math.pow((x1-x3),2)+Math.pow((y1-y3),2),0.5))/2
-        System.out.println(Math.pow(s*(s-Math.pow(Math.pow((x2-x1),2)+Math.pow((y2-y1),2),0.5)*(s-Math.pow(Math.pow((x3-x2),2)+Math.pow((y3-y2),2),0.5))*(s-Math.pow(Math.pow((x1-x3),2)+Math.pow((y1-y3),2),0.5))),0.5)
+        double s1 = Math.pow(Math.pow((x2-x1),2)+Math.pow((y2-y1),2),0.5);
+        double s2 = Math.pow(Math.pow((x3-x2),2)+Math.pow((y3-y2),2),0.5);
+        double s3 = Math.pow(Math.pow((x1-x3),2)+Math.pow((y1-y3),2),0.5);
+        double s = (s1+s2+s3)/2;
+        System.out.println(Math.pow(s*(s-s1)*(s-s2)*(s-s3),0.5));
     }
 }
