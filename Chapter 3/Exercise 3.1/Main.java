5451106289931eb1934c32737c36d46001eb8b53
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-07 11:15:49 +0600

    Main class

--- Chapter 3/Exercise 3.1/Main.java
+++ Chapter 3/Exercise 3.1/Main.java
@@ -2,11 +2,19 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);   
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter a, b, c: ");
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
-        System.out.println("The equation has two roots "+ ((-1)*b+Math.pow(b*b-4*a*c))/(2*a) + " and " + ((-1)*b-Math.pow(b*b-4*a*c))/(2*a);
+        if(b*b-4*a*c == 0){
+            System.out.println("The equation has one root" + ((-1)*b+Math.pow(b*b-4*a*c, 0.5))/(2*a));
+        }
+        else if(b*b-4*a*c > 0){
+            System.out.println("The equation has two roots "+ ((-1)*b+Math.pow(b*b-4*a*c, 0.5))/(2*a) + " and " + ((-1)*b-Math.pow(b*b-4*a*c, 0.5))/(2*a));
+        }
+        else{
+            System.out.println("The equation has no real roots");
+        }
     }
 }
