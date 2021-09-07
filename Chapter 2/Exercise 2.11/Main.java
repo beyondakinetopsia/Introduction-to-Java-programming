c948b6e9f3ed678ced3fca970eb79812f5a45bdf
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-07 12:29:05 +0600

    Main class

--- Chapter 2/Exercise 2.11/Main.java
+++ Chapter 2/Exercise 2.11/Main.java
@@ -7,12 +7,12 @@ public class Main{
         double seconds = 365*24*60*60;
         System.out.print("Enter the number of years: ");
         int num = input.nextInt();
-        int birthPerYear = (int) seconds/7;
-        int deathPerYear = (int) seconds/13;
-        int imigrationPerYear = (int) seconds/45;
+        double birthPerYear = seconds/7;
+        double deathPerYear = seconds/13;
+        double imigrationPerYear = seconds/45;
         for(int i=0;i<num;i++){
             pop+=birthPerYear+imigrationPerYear-deathPerYear;
         }
-        System.out.println("he population in "+num+" years is "+pop);
+        System.out.println("The population in "+num+" years is "+(int) pop);
     }
 }
