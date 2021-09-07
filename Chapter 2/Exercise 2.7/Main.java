b06ab857311a447fa65978d413f1991069d4c304
beyondakinetopsia <90122769+beyondakinetopsia@users.noreply.github.com>
2021-09-07 12:25:29 +0600

    Main class

--- Chapter 2/Exercise 2.7/Main.java
+++ Chapter 2/Exercise 2.7/Main.java
@@ -4,6 +4,6 @@ public class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         double num = input.nextDouble();
-        System.out.println(num + "minutes is approximately" + num/(60*24*365) + " and " + (num%(60*24*365))/(60*24) + " days");
+        System.out.println((int)num + " minutes is approximately " + (int)num/(60*24*365) + " years and " + (int)(num%(60*24*365))/(60*24) + " days");
     }
 }
